7d1e968ba19dee23e0902c55987769f27060ffe0
SamyrOR <saamyr@live.com>
2022-11-06 11:13:47 -0300

    feat: comments and escapes in C

---
+++ c/comments-and-escapes.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+	
+	//This is a comment	
+	/*
+		This 
+		is 
+		a 
+		multiline
+		comment
+	*/
+	//This is escape characters 
+	printf("1\t2\t3\n4\t5\t6");
+	return 0;
+};
