c221bf58c8d38d93db4946f411ecd6467956e4ca
SamyrOR <saamyr@live.com>
2022-11-30 23:02:38 -0300

    feat: functions in C

---
+++ c/functions.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+void birthday() {
+
+	printf("\n Happy birthday to you!");
+	printf("\n Happy birthday to you!");
+	printf("\n Happy birthday dear... YOU!");
+	printf("\n Happy birthday to you!");
+
+}
+
+int main() {
+
+	birthday(); 
+	birthday(); 
+	birthday(); 
+
+ return 0;
+}
