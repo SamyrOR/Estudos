4ae0f4a82dbb9741030b04ff81c476d12517b8b5
SamyrOR <saamyr@live.com>
2023-11-11 21:52:01 -0300

    fead: added mix project example

---
+++ elixir-school/mix_10/lib/mix10.ex
@@ -0,0 +1,18 @@
+defmodule Mix10 do
+  @moduledoc """
+  Documentation for `Mix10`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Mix10.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ elixir-school/mix_10/test/mix10_test.exs
@@ -0,0 +1,8 @@
+defmodule Mix10Test do
+  use ExUnit.Case
+  doctest Mix10
+
+  test "greets the world" do
+    assert Mix10.hello() == :world
+  end
+end
