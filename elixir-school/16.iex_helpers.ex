c7064d7af9e4a861ab8f8f77f5885db51e97c777
SamyrOR <saamyr@live.com>
2023-11-12 22:03:58 -0300

    feat: added iex_helpers

---
+++ elixir-school/16.iex_helpers.ex
@@ -0,0 +1,16 @@
+#iex is the REPL of elixir, we can run easily
+#just type iex and its start
+#to use the autocomplete, press tab
+#to define a custom configuration file, create a new .iex.exs
+
+#h module_name
+#will return the doc of the module
+
+#i module_name
+#will return the information of the module
+#data type, bytecode, location, source, version, compile, description
+
+#r module_name
+#recompile the module
+
+#t return the data type module
